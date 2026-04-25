24ae7760ba041d7337d7d02fcc2512aeb32f8193
Andrea Seijo <andreasn783@gmail.com>
2026-04-25 21:29:12 +0200

    Nueva clase Division e integración en la Calculadora

--- Calculadora-Contornos/src/Calculadora.java
+++ Calculadora-Contornos/src/Calculadora.java
@@ -2,9 +2,11 @@ public class Calculadora {
     public static void main(String[] args) {
         Suma s = new Suma();
         Resta r = new Resta();
+        Division d = new Division();
 
         System.out.println("=== CALCULADORA JAVA ===");
         System.out.println("Resultado suma = " + s.sumar(10,5));
         System.out.println("Resultado resta = " + r.restar(10,5));
+        System.out.println("Resultado división = " + d.dividir(10,5));
     }
 }
