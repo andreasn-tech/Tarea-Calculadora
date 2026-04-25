edaf0e47bbf792797270a8a84dd1ca3e8792e884
Andrea Seijo <andreasn783@gmail.com>
2026-04-25 21:14:31 +0200

    Nueva clase Multiplicacion e integración en la Calculadora

--- Calculadora-Contornos/src/Calculadora.java
+++ Calculadora-Contornos/src/Calculadora.java
@@ -7,6 +7,6 @@ public class Calculadora {
         System.out.println("=== CALCULADORA JAVA ===");
         System.out.println("Resultado suma = " + s.sumar(10,5));
         System.out.println("Resultado resta = " + r.restar(10,5));
-        System.out.println("Resultado multiplicación = " + r.restar(10,5));
+        System.out.println("Resultado multiplicación = " + m.multiplicar(10,5));
     }
 }
